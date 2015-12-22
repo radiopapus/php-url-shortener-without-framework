84a29ce828cd3abb049912cb20f152420edcd52c
ViktorZharina <zharina_vv@nts.su>
2015-12-22 15:37:57 +0600

    php7dev

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = array();
-$config['site'] = 'http://local.xiag.ru/';
+$config['site'] = 'http://php7dev/';
 $config['possibleChars'] = 'abcdefghijklmnopqrstuvwxyz';
 $config['dsn'] = 'mysql:host=localhost;dbname=urlshortener';
 $config['db_user'] = 'root';
