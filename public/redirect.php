6c8d4cffda2b45a4526d78b5f70948836f2ca783
ViktorZharina <zharina_vv@nts.su>
2015-12-22 14:10:01 +0600

    remove validation of url.

--- public/redirect.php
+++ public/redirect.php
@@ -6,8 +6,8 @@ require_once('../classes/UrlShortenerModel.php');
 if (isset($_GET['hash'])) {
     $shortener = new UrlShortenerModel($config);
     $id = $shortener->getIdByHash($_GET['hash']);
-    $srcUrl = $shortener->getSourceUrlById($id, array('srcurl'));
+    $srcUrl = $shortener->getSourceUrlById($id);
     header('HTTP/1.1 301 Moved Permanently');
     header('Location: ' .  $srcUrl);
-    return;    
+    return;
 }
