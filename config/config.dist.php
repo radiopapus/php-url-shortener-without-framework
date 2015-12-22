0566d079e9fe34b8a8aba16ebbcb31e40df57c77
ViktorZharina <zharina_vv@nts.su>
2015-12-22 14:11:33 +0600

    config.dist

---
+++ config/config.dist.php
@@ -0,0 +1,8 @@
+<?php
+
+$config = array();
+$config['site'] = 'http://local.xiag.ru/';
+$config['possibleChars'] = 'abcdefghijklmnopqrstuvwxyz';
+$config['dsn'] = 'mysql:host=localhost;dbname=url_shortener';
+$config['db_user'] = 'root';
+$config['db_pass'] = 'toor';
