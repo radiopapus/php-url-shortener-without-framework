820e4a322330c2b26d8dc1abf2fe7751f11a57f4
ViktorZharina <zharina_vv@nts.su>
2015-12-22 14:10:20 +0600

    remove validator from code

--- public/urlshortener.php
+++ public/urlshortener.php
@@ -9,27 +9,24 @@
  */
 
 /**
+ * @todo  
+ * @todo  Exception error handler
  * @todo  не сохранять ссылки 404 = 0
  */
 
-namespace ViktorZharina\UrlShortener;
 require_once('../config/config.php');
 require_once('../classes/UrlShortenerModel.php');
+require_once('../classes/exceptions/UrlShortenerException.php');
 require_once('../classes/validators/Validator.php');
 require_once('../classes/Response.php');
 
 if (isset($_REQUEST['url']) AND !empty($_REQUEST['url'])) {
     $url = trim($_REQUEST['url'], '!"#$%&\'()*+,-./@:;<=>[\\]^_`{|}~');
     $response = new Response();
-    $result = array('success' => 0, 'shortUrl' => '', 'errMsg' => 'Wrong url');
-    if (Validator::url($url)) {
-        $shortener = new UrlShortenerModel($config);
-        $insertId = $shortener->add($url);
-        $shortUrl = $shortener->getShortUrl($insertId);
-        $result = array('success' => 1, 'shortUrl' => $shortUrl, 'errMsg' => '');
-        echo $response->returnJson($result);
-        exit;
-    }
-
+    $shortener = new UrlShortenerModel($config);
+    $insertId = $shortener->add($url);
+    $shortUrl = $shortener->getShortUrl($insertId);
+    $result = array('success' => 1, 'shortUrl' => $shortUrl, 'errMsg' => '');
     echo $response->returnJson($result);
+    exit;
 }
