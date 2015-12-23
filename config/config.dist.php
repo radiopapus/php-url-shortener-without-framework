b0aef65e0c94e4a641dd9569380c7519ba1f2a96
ViktorZharina <zharina_vv@nts.su>
2015-12-23 09:10:20 +0600

    add db_opt params

--- config/config.dist.php
+++ config/config.dist.php
@@ -6,3 +6,8 @@ $config['possibleChars'] = 'abcdefghijklmnopqrstuvwxyz';
 $config['dsn'] = 'mysql:host=localhost;dbname=urlshortener';
 $config['db_user'] = 'root';
 $config['db_pass'] = 'toor';
+$config['db_opt'] = array(
+                    \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
+                    \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
+                    \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+                );
