06c0f24a63bc99140fec221cb4a695e87e466251
ViktorZharina <zharina_vv@nts.su>
2015-12-23 13:46:08 +0600

    url not defined error

--- public/urlshortener.php
+++ public/urlshortener.php
@@ -1,28 +1,13 @@
 <?php
-/**
- * Website: http://viktor.zharina.info
- *
- * Licensed under The MIT License
- *
- * @author ViktorZharina <viktorz1986@gmail.com>
- * @version 0.1
- */
-
-/**
- * @todo  
- * @todo  Exception error handler
- * @todo  не сохранять ссылки 404 = 0
- */
-
 require_once('../config/config.php');
 require_once('../classes/UrlShortenerModel.php');
 require_once('../classes/exceptions/UrlShortenerException.php');
-require_once('../classes/validators/Validator.php');
 require_once('../classes/Response.php');
+
+$response = new Response();
 
 if (isset($_REQUEST['url']) AND !empty($_REQUEST['url'])) {
     $url = trim($_REQUEST['url'], '!"#$%&\'()*+,-./@:;<=>[\\]^_`{|}~');
-    $response = new Response();
     $pdo = new \PDO($config['dsn'], $config['db_user'], $config['db_pass'], $config['db_opt']);
     $shortener = new UrlShortenerModel($pdo, $config);
     $insertId = $shortener->add($url);
@@ -31,3 +16,6 @@ if (isset($_REQUEST['url']) AND !empty($_REQUEST['url'])) {
     echo $response->returnJson($result);
     exit;
 }
+
+$result = array('success' => 0, 'shortUrl' => '', 'errMsg' => 'Url not defined');
+echo $response->returnJson($result);
